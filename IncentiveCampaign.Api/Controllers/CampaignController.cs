784a91c0c83d611d97b3a09592787bc36f951527
emerson <emersonej1@gmail.com>
2017-04-12 00:03:50 -0300

    upload all again

--- IncentiveCampaign.Api/Controllers/CampaignController.cs
+++ IncentiveCampaign.Api/Controllers/CampaignController.cs
@@ -17,6 +17,7 @@ using IncentiveCampaign.Api.Models.Dealership;
 
 namespace IncentiveCampaign.Api.Controllers
 {
+    //[RoutePrefix("api/campaign")]
     public class CampaignController : ApiController
     {
         private readonly IIncentiveCampaignApl incentiveCampaignApl;
